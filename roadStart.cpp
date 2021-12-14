490df097ae6f7b9d3e9b963173d88ece1570ef08
Marcelo Carvalho <marcelo.carvalho@fabamaq.com>
2021-12-14 15:01:13 +0000

    New Excercise c++

--- roadStart.cpp
+++ roadStart.cpp
@@ -2,6 +2,12 @@
 int main()
 {
     using namespace std;
-    cout << "What's up, Doc! \n";
+    //Compile >> g++ roadStart.cpp 
+    // Run ./a.out
+    cout << "Come up and C++ me some time. ";
+    cout << endl;
+    cout << "You won't regret it! " << endl;
+    cout << int('Q') << endl;
+    cout << static_cast<long> ('Q') << endl;
     return 0;
 }
